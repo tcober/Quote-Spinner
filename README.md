ca64c0bfcf133806a230d7ff85b422e873b4879d
tcober <tcober5@gmail.com>
2016-06-26 22:16:53 -0700

    Adding the js file and demo html

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Quote-Spinner
-A simple quote spinner jquery plugin
+A simple quote spinner jquery plugin  
+Checkout the demo <a href="http://boxboxboxbox.com/quotespinner">here</a>
---
+++ quotespinner.js
@@ -0,0 +1,41 @@
+var spinner = {
+    index: 0,
+    auto: function(currentIndex) {
+      if (currentIndex != undefined) {
+        spinner.index = currentIndex % spinner.quotes.length;
+      } else {
+        spinner.index = (spinner.index + 1) % spinner.quotes.length;
+      }
+      spinner.quotes.removeClass("show");
+      $(spinner.quotes[spinner.index]).addClass("show");
+      spinner.dots.removeClass('dot-fill');
+      $(spinner.dots[spinner.index]).addClass('dot-fill');
+    },
+
+    initial: function(){
+      this.quotes = $(".quote-rotate");
+      this.images = $(".quote-image");
+      spinner.quotes.first().addClass("show");
+      //dots
+      for (i = 0; i < spinner.quotes.length; i++) {
+        $('.quote-dots').append('<div class="nav-dot"></div>');
+      }
+      this.dots = $(".nav-dot");
+      $(spinner.dots).first().addClass('dot-fill');
+    },
+
+    dotnav: function(){
+      $(spinner.dots).on("click", function(){
+        var currentIndex = $(spinner.dots).index(this);
+        spinner.auto(currentIndex);
+        window.clearInterval(interval);
+        interval = window.setInterval(spinner.auto, 6500);
+      });
+    }
+  }
+
+$(document).ready(function() {
+  spinner.initial();
+  spinner.dotnav();
+  interval = window.setInterval(spinner.auto, 4500);
+});
